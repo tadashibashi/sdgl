2b7e7d0ff537a16f78847fcffe7d4799ece776aa
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-04-08 15:10:46 -0700

    Add line number and file name to GL error macro

--- sdgl/gl.h
+++ sdgl/gl.h
@@ -10,7 +10,7 @@
 #include <stdexcept>
 #define GL_ERR_CHECK() do { if (glGetError() != GL_NO_ERROR) \
 { \
-SDGL_ERROR("GL Error: {}", glGetError()); \
+SDGL_ERROR("GL Error: {}: on line `{}`, in function `{}`, in file \"{}\"", glGetError(), __LINE__, __func__, __FILE__); \
 throw std::runtime_error("GL Error"); \
 } } while(false)
 #else
