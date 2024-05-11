d51f3ad2cc88eead62f34d05a585fe17d42ec18d
Aaron Ishibashi <a.ishibashi.music@gmail.com>
2024-05-11 11:36:33 -0700

    simplify controller map copy by running command during generation instead of setting up a target / dependency

--- lib/SDL_GameControllerDB.cmake
+++ lib/SDL_GameControllerDB.cmake
@@ -7,16 +7,12 @@ function(copy_gamepad_mappings)
     endif()
 
     set (MAPFILE ${CMAKE_CURRENT_BINARY_DIR}/gamecontrollerdb.txt)
-    add_custom_command(OUTPUT ${MAPFILE}
+    execute_process(
         COMMAND ${CMAKE_COMMAND} -E copy_if_different
             ${SDGL_ROOT_DIR}/lib/SDL_GameControllerDB/gamecontrollerdb.txt ${MAPFILE})
-    add_custom_target(${TARGET_NAME}-copy-gamepad-mapping
-        DEPENDS ${MAPFILE})
-    add_dependencies(${TARGET_NAME} ${TARGET_NAME}-copy-gamepad-mapping)
 
     # link mappings file into emscripten .data file
     if (EMSCRIPTEN)
         target_link_options(${TARGET_NAME} PRIVATE --preload-file ${MAP_FILE})
     endif()
-
 endfunction()
